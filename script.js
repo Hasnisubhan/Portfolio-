ce931b97a59b998234efcef1e89bbcee2d491d12
Hasni Subhan <hasnisubhan@gmail.com>
2026-04-03 21:36:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,6 +84,14 @@ function openModalWithInfo(src, caption = "") {
   }
 }
 
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 800); // small delay for smooth feel
+});
+
 
 // ================= IMAGE SLIDER =================
 let currentIndex = 0;
